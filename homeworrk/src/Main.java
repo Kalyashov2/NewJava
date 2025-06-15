b2f5d69c546fbf2c2688c712fa9572643ea8d202
Иван <kalyashov-ivan@yandex.ru>
2025-06-15 19:53:56 +0300

    homework

---
+++ homeworrk/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        var frog = 3.5;
+        System.out.println(frog);
+        frog = frog*10;
+        System.out.println(frog);
+        frog = frog / 3.5;
+        System.out.println(frog);
+        frog = frog+4;
+        System.out.println(frog);
+        }
+    }
